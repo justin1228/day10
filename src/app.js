62d698b4880812fa27d6d113ac3e1c8f47ec70c2
justin1228 <didls@hanyang.ac.kr>
2020-04-29 14:10:00 +0900

    "randomGame"

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const generate = document.getElementById("generateNum");
+console.log(generate);
+const iR = document.getElementById("inputRange");
+const iN = document.getElementById("guessNum");
+const sM = document.getElementById("submitButton");
+const result = document.getElementById("result");
+const result_1 = document.getElementById("result_1");
+var num1 = 0;
+function makeRand(a,b){
+    var Num = Math.floor(Math.random()*(a-b)+b);
+
+    return Num;
+}
+
+function handleClick(event){
+   
+    console.log(iN.value);
+    var iVal = iR.value;
+    var randNum = makeRand(iVal,0);
+    num1 = randNum;
+    
+    result.innerHTML = `You: ${iN.value} <br> Computer: ${num1}`;
+    if(iN.value === num1){
+        result_1.innerHTML = "You Win!";
+    }
+    else(
+        result_1.innerHTML = "You Lose!"
+    )
+}
+function handleOnchange(){
+    var iVal = iR.value;
+    generate.innerHTML = `Generate number between 0 and ${iVal}`;
+    
+    sM.addEventListener("click",handleClick);
+}
+
+function init(){
+    iR.addEventListener("input", handleOnchange);
+}
+
+init();
